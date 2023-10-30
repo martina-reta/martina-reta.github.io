922fa763ef3c151bc92602b056613932f66a9eb9
Martina Reta <martuh8v@gmail.com>
2023-10-30 04:08:53 -0300

    funcionalidades tickets

--- scripts/tickets.js
+++ scripts/tickets.js
@@ -1,15 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //acá agarro los elementos del DOM para usarlos
     const cantidadInput = document.querySelector('.cantidad');
     const totalElement = document.getElementById('total');
+    const categoryInput = document.querySelector('.category');
+    const formulario = document.querySelector('form');
 
     const precioTicket = 200;
+    const descuentos = {
+        1: 80, // Estudiante
+        2: 50, // Trainee
+        3: 15  // Junior
+    };
+
+    function aplicarDescuento(precio, categoria) {
+        return precio * (descuentos[categoria] / 100);
+    }
 
     function actualizarTotal() {
         const cantidad = cantidadInput.value ? parseInt(cantidadInput.value) : 0;
-        const total = cantidad * precioTicket;
+        const categoria = categoryInput.value;
+        const precioConDescuento = precioTicket - aplicarDescuento(precioTicket, categoria);
+        const total = cantidad * precioConDescuento;
         totalElement.textContent = total;
     }
 
-    cantidadInput.addEventListener('input', actualizarTotal);
+    const botonResumen = document.querySelector('.btn-resumen');
+    botonResumen.addEventListener('click', function () {
+        actualizarTotal();
+    });
+
+    const botonBorrar = document.querySelector('.btn-borrar');
+    botonBorrar.addEventListener('click', function () {
+        formulario.reset();
+        totalElement.textContent = '0';
+    });
 });
